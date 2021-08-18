493b8a416d9936a794019d5dbe1b1e8d0b30daa3
sahil rohilla <sahilrohilla03@gmail.com>
2021-08-19 05:30:29 +0700

    fb login 1

--- main/urls.py
+++ main/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', views.home),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'templates/index.html')
+    return render(request, 'main/index.html')
