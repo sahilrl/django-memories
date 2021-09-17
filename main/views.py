bb6ee1e51756d2ad59c55b3c06de9e584333113c
sahilrl <sahilrohilla03@gmail.com>
2021-09-18 03:47:18 +0700

    got user profile

--- main/views.py
+++ main/views.py
@@ -19,7 +19,7 @@ def login_facebook(request):
 
     if (request.get_full_path_info() == "/login_facebook/"):
         print(request)
-        return HttpResponseRedirect(f'https://www.facebook.com/v11.0/dialog/oauth?client_id={app_id}&redirect_uri=http://localhost:8000/login_facebook/&state={"{st=state123abc,ds=123456789}&scope=email,public_profile"}')
+        return HttpResponseRedirect(f'https://www.facebook.com/v11.0/dialog/oauth?client_id={app_id}&redirect_uri=http://localhost:8000/login_facebook/&state={"{st=state123abc,ds=123456789}&scope=email"}')
     else:
         print(request)
         req = urlparse(request.get_full_path_info())
@@ -27,20 +27,19 @@ def login_facebook(request):
         req = req[0].replace('code=','')
 
         access_token = requests.get(f'https://graph.facebook.com/v11.0/oauth/access_token?client_id={app_id}&redirect_uri=http://localhost:8000/login_facebook/&client_secret={app_secret}&code={req}')
-        access_token = access_token.json()
-        access_token = access_token.get("access_token")
-        # print(f"The access token is {access_token}")
+        access_token = access_token.json().get("access_token")
     
         app_access_token = requests.get(f"https://graph.facebook.com/oauth/access_token?client_id={app_id}&client_secret={app_secret}&grant_type=client_credentials")
-        app_access_token = app_access_token.json()
-        # print(app_access_token)
-        app_access_token = app_access_token.get("access_token")
-        # print('This is the app access token',app_access_token,'\n')
+        app_access_token = app_access_token.json().get("access_token")
 
         check = requests.get(f'https://graph.facebook.com/debug_token?input_token={access_token}&access_token={app_access_token}')
-        user_id = check.json()
-        user_id = user_id.get("data")["user_id"]
-        # print(user_id)
-        profile = requests.get(f'https://graph.facebook.com/{user_id}?fields=id,name,email,picture&access_token={access_token}&client_secret={app_secret}&client_id={app_id}')
-        print(profile.json())
-        return HttpResponse('done')
+        user_id = check.json().get("data")["user_id"]
+
+        profile = requests.get(f'https://graph.facebook.com/{user_id}?fields=id,name,email,picture.width(400).height(400)&access_token={access_token}&client_secret={app_secret}&client_id={app_id}')
+        profile = profile.json()
+        try:
+            name, email, picture = profile['name'], profile['email'], profile['picture'].get('data')['url']
+            dict = {name: 'name', email: 'email', picture: 'picture'}
+        except:
+            pass
+        return render(request,'main/app.html', dict )
